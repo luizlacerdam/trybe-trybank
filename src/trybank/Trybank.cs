5c201a9facb4f559fee2cf2e5336b690baf5194f
luizlacerdam <luizlacerdam.dev@gmail.com>
2024-05-29 20:48:40 -0400

    :sparkles: transfer

--- src/trybank/Trybank.cs
+++ src/trybank/Trybank.cs
@@ -116,8 +116,22 @@ public class TrybankLib
     // 7. Construa a funcionalidade de transferir dinheiro entre contas
     public void Transfer(int destinationNumber, int destinationAgency, int value)
     {
-        throw new NotImplementedException();
+        if (!Logged)
+        {
+            throw new AccessViolationException("Usuário não está logado");
+        }
+        if (Bank[loggedUser, 3] < value)
+        {
+            throw new InvalidOperationException("Saldo insuficiente");
+        }
+        for (int i = 0; i < registeredAccounts; i++)
+        {
+            if (Bank[i, 0] == destinationNumber && Bank[i, 1] == destinationAgency)
+            {
+                Bank[loggedUser, 3] -= value;
+                Bank[i, 3] += value;
+                break;
+            }
+        }
     }
-
-
 }
